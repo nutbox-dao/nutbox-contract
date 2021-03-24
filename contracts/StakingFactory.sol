6595455cb8f1cb8090466573ee6e9f30a150c318
Wenfeng Wang <kalot.wang@gmail.com>
2021-03-24 22:53:07 +0800

    Add StakingFactory

--- contracts/StakingFactory.sol
+++ contracts/StakingFactory.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.6.0 <0.8.0;
+pragma experimental ABIEncoderV2;
+
+import './libraries/Types.sol';
+import './StakingTemplate.sol';
 
 /**
  * @dev Factory contract to create an StakingTemplate entity
@@ -8,5 +12,42 @@ pragma solidity >=0.6.0 <0.8.0;
  * This is the entry contract that user start to create their own staking economy.
  */
 contract StakingFactory {
+
+    address public feeAddress;
+
+    event StakingFeastCreated(address indexed creater, address stakingFeast, address rewardToken);
+
+    constructor(address _feeAddress) {
+        feeAddress = _feeAddress;
+    }
+
+    function createStakingFeast (
+        NutboxERC20 _rewardToken,
+        Types.Distribution[] memory _distributionEras,
+        Types.EndowedAccount[] memory _endowedAccounts
+    ) public {
+        require(address(_rewardToken) != address(0), 'Invalid reward token address');
+        require(_distributionEras.length > 0, 'Should give at least one distribution');
+
+        address feastAddress;
+        bytes memory bytecode = type(StakingTemplate).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(_rewardToken));
+        assembly {
+            feastAddress := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        StakingTemplate(feastAddress).initialize(
+            msg.sender,
+            _rewardToken,
+            _distributionEras,
+            _endowedAccounts
+        );
+
+        emit StakingFeastCreated(msg.sender, feastAddress, address(_rewardToken));
+    }
+
+    function setFeeAddress(address _feeAddress) public {
+        require(msg.sender == feeAddress, 'Permission denied to set fee address');
+        feeAddress = _feeAddress;
+    }
 
 }
