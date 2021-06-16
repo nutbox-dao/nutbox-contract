e1818bce715cab146afb4061cd53ae4d2057bc7e
Wenfeng Wang <kalot.wang@gmail.com>
2021-06-16 09:40:12 +0800

    Add scripts to deploy contracts

--- README.md
+++ README.md
@@ -1,19 +1,69 @@
 # Nutbox Contract
 
-# Local Development
+## Local Development
 
-## Prerequirements
+### Prerequirements
 
 with ```node```/```truffle```/```ganache``` installed.
 
-## Install Dependencies
+### Install Dependencies
 
-`npm install`
+`yarn`
 
-## Compile Contracts
+### Compile Contracts
 
-`npm compile`
+`truffle compile`
 
-## Run Tests
+### Run Tests
 
-`npm test`
+`truffle test`
+
+### Deploy contract
+
+Run script ```scripts/deploy.js```:
+
+```sh
+ENDPOINT=http://localhost:8545 KEY=<private key> GASLIMIT=10000000 GASPRICE=50000000000 node deploy.js
+```
+
+You should see the output like below:
+
+```sh
+================================================================
+Url:        http://localhost:8545
+Deployer:   0x03bAb49C10c93f123eeE13b82adc55d1B07C9fb2
+Gas Limit:   10000000
+Gas Price:   50000000000
+Deploy Cost: 0.8435647
+
+Options
+=======
+Bridge Fee:     0
+Bridge Expiry:  10
+Fee Addr:       0x03bAb49C10c93f123eeE13b82adc55d1B07C9fb2
+
+Contract Addresses
+================================================================
+RegistryHub:                        0x30E0b89a526f33395c2b560724b071B3AF158E2c
+----------------------------------------------------------------
+HomeChainAssetRegistry:             0xecF6B570C569dB9858422d1a9E5C93FDb94D937f
+----------------------------------------------------------------
+SteemHiveDelegateAssetRegistry:     0x8fCD5AD701b7c1c60dc8548e1492b4Bf0E686758
+----------------------------------------------------------------
+SubstrateCrowdloanAssetRegistry:    0x51F6a8ef4237b45E771328e6E9E0C977EF295EC0
+----------------------------------------------------------------
+SubstrateNominateAssetRegistry:     0x7FEBAc047CF3d2538F1Ea5Cd932E4ee66A914536
+----------------------------------------------------------------
+ERC20AssetHandler:                  0x53212181581FCa0f65Db352a15775486dF338F3C
+----------------------------------------------------------------
+ERC721AssetHandler:                 Not Deployed
+----------------------------------------------------------------
+TrustlessAssetHandler:              0xF3746ef2cF02f291da86649B87e773232319f089
+----------------------------------------------------------------
+Executor:                           0x41a6bE22D454Aa8252c34a511F2Cd2891172eF23
+----------------------------------------------------------------
+Bridge:                             0xf697A0E388f0b3322eC454a15E8FD828851b7073
+----------------------------------------------------------------
+StakingFactory:                     0xd679D28925bFD2BBD162d94193FAA87e3C15eC74
+================================================================
+```
---
+++ scripts/deploy.js
@@ -0,0 +1,233 @@
+// USAGE: 
+//  ENDPOINT=http://localhost:8545 KEY=<private key> GASLIMIT=10000000 GASPRICE=50000000000 node deploy.js
+
+require('dotenv').config();
+const ethers = require('ethers');
+
+const ERC20AssetHandlerJson = require('../build/contracts/ERC20AssetHandler.json');
+const TrustlessAssetHandlerJson = require('../build/contracts/TrustlessAssetHandler.json');
+const RegistryHubJson = require('../build/contracts/RegistryHub.json');
+const HomeChainAssetRegistryJson = require('../build/contracts/HomeChainAssetRegistry.json');
+const SteemHiveDelegateAssetRegistryJson = require('../build/contracts/SteemHiveDelegateAssetRegistry.json');
+const SubstrateCrowdloanAssetRegistryJson = require('../build/contracts/SubstrateCrowdloanAssetRegistry.json');
+const SubstrateNominateAssetRegistryJson = require('../build/contracts/SubstrateNominateAssetRegistry.json');
+const StakingFactoryJson = require('../build/contracts/StakingFactory.json');
+const ExectorJson = require('../build/contracts/Executor.json');
+const BridgeJson = require('../build/contracts/Bridge.json');
+const SimpleERC20Json = require('../build/contracts/SimpleERC20.json');
+
+async function deployRegistryHubContract(env) {
+    let factory = new ethers.ContractFactory(RegistryHubJson.abi, RegistryHubJson.bytecode, env.wallet);
+    let contract = await factory.deploy(
+        { gasPrice: env.gasPrice, gasLimit: env.gasLimit}
+    );
+    await contract.deployed();
+    env.registryHubContract = contract.address;
+    console.log("✓ RegistryHub contract deployed");
+}
+
+async function deployHomeChainAssetRegistryContract(env) {
+    let factory = new ethers.ContractFactory(HomeChainAssetRegistryJson.abi, HomeChainAssetRegistryJson.bytecode, env.wallet);
+    let contract = await factory.deploy(
+        env.registryHubContract,
+        { gasPrice: env.gasPrice, gasLimit: env.gasLimit}
+    );
+    await contract.deployed();
+    env.homeChainAssetRegistryContract = contract.address;
+    console.log("✓ HomeChainAssetRegistry contract deployed");
+}
+
+async function deploySteemHiveDelegateAssetRegistryContract(env) {
+    let factory = new ethers.ContractFactory(SteemHiveDelegateAssetRegistryJson.abi, SteemHiveDelegateAssetRegistryJson.bytecode, env.wallet);
+    let contract = await factory.deploy(
+        env.registryHubContract,
+        { gasPrice: env.gasPrice, gasLimit: env.gasLimit}
+    );
+    await contract.deployed();
+    env.steemHiveDelegateAssetRegistryContract = contract.address;
+    console.log("✓ SteemHiveDelegateAssetRegistry contract deployed");
+}
+
+async function deploySubstrateCrowdloanAssetRegistryContract(env) {
+    let factory = new ethers.ContractFactory(SubstrateCrowdloanAssetRegistryJson.abi, SubstrateCrowdloanAssetRegistryJson.bytecode, env.wallet);
+    let contract = await factory.deploy(
+        env.registryHubContract,
+        { gasPrice: env.gasPrice, gasLimit: env.gasLimit}
+    );
+    await contract.deployed();
+    env.substrateCrowdloanAssetRegistryContract = contract.address;
+    console.log("✓ SubstrateCrowdloanAssetRegistry contract deployed");
+}
+
+async function deploySubstrateNominateAssetRegistryContract(env) {
+    let factory = new ethers.ContractFactory(SubstrateNominateAssetRegistryJson.abi, SubstrateNominateAssetRegistryJson.bytecode, env.wallet);
+    let contract = await factory.deploy(
+        env.registryHubContract,
+        { gasPrice: env.gasPrice, gasLimit: env.gasLimit}
+    );
+    await contract.deployed();
+    env.substrateNominateAssetRegistryContract = contract.address;
+    console.log("✓ SubstrateNominateAssetRegistry contract deployed");
+}
+
+async function deployExectorContract(env) {
+    let factory = new ethers.ContractFactory(ExectorJson.abi, ExectorJson.bytecode, env.wallet);
+    let contract = await factory.deploy(
+        env.registryHubContract,
+        { gasPrice: env.gasPrice, gasLimit: env.gasLimit}
+    );
+    await contract.deployed();
+    env.exectorContract = contract.address;
+    console.log("✓ Proposal Exector contract deployed");
+}
+
+async function deployBridgeContract(env) {
+    let factory = new ethers.ContractFactory(BridgeJson.abi, BridgeJson.bytecode, env.wallet);
+    let contract = await factory.deploy(
+        env.registryHubContract,
+        env.exectorContract,
+        ethers.utils.parseEther(env.bridgeFee.toString()),
+        env.bridgeExpiry,
+        { gasPrice: env.gasPrice, gasLimit: env.gasLimit}
+
+    );
+    await contract.deployed();
+    env.bridgeContract = contract.address;
+    console.log("✓ Bridge contract deployed");
+}
+
+async function deployERC20AssetHandlerContract(env) {
+    let factory = new ethers.ContractFactory(ERC20AssetHandlerJson.abi, ERC20AssetHandlerJson.bytecode, env.wallet);
+    let contract = await factory.deploy(
+        env.registryHubContract,
+        { gasPrice: env.gasPrice, gasLimit: env.gasLimit}
+
+    );
+    await contract.deployed();
+    env.erc20AssetHandlerContract = contract.address;
+    console.log("✓ ERC20AssetHandler contract deployed");
+}
+
+async function deployTrustlessAssetHandlerContract(env) {
+    let factory = new ethers.ContractFactory(TrustlessAssetHandlerJson.abi, TrustlessAssetHandlerJson.bytecode, env.wallet);
+    let contract = await factory.deploy(
+        env.registryHubContract,
+        env.bridgeContract,
+        { gasPrice: env.gasPrice, gasLimit: env.gasLimit}
+    );
+    await contract.deployed();
+    env.trustlessAssetHandlerContract = contract.address;
+    console.log("✓ TrustlessAssetHandler contract deployed");
+}
+
+async function deployStakingFactoryContract(env) {
+    let factory = new ethers.ContractFactory(StakingFactoryJson.abi, StakingFactoryJson.bytecode, env.wallet);
+    let contract = await factory.deploy(
+        env.registryHubContract,
+        env.feeAddr,
+        { gasPrice: env.gasPrice, gasLimit: env.gasLimit}
+
+    );
+    await contract.deployed();
+    env.stakingFactoryContract = contract.address;
+    console.log("✓ StakingFactory contract deployed");
+}
+
+async function deployERC20(env) {
+    const factory = new ethers.ContractFactory(SimpleERC20Json.abi, SimpleERC20Json.bytecode, env.wallet);
+    const contract = await factory.deploy("", "", { gasPrice: env.gasPrice, gasLimit: env.gasLimit});
+    await contract.deployed();
+    env.simpleERC20Contract = contract.address;
+    console.log("✓ Simple ERC20 contract deployed");
+}
+
+async function main() {
+    let env = {};
+    env.url = process.env.ENDPOINT || 'http://localhost:8545';
+    env.privateKey = process.env.KEY;
+    env.provider = new ethers.providers.JsonRpcProvider(env.url);
+    env.wallet = new ethers.Wallet(env.privateKey, env.provider);
+    env.gasLimit = ethers.utils.hexlify(Number(process.env.GASLIMIT));
+    env.gasPrice = ethers.utils.hexlify(Number(process.env.GASPRICE));
+
+    // hardcode
+    env.bridgeFee = 0;
+    env.bridgeExpiry = 10;
+    env.feeAddr = env.wallet.address;
+
+    let startBalance = await env.provider.getBalance(env.wallet.address)
+
+    // deploy asset contracts
+    await deployRegistryHubContract(env);
+    await deployHomeChainAssetRegistryContract(env);
+    await deploySteemHiveDelegateAssetRegistryContract(env);
+    await deploySubstrateCrowdloanAssetRegistryContract(env);
+    await deploySubstrateNominateAssetRegistryContract(env);
+
+    // deploy bridge contract
+    await deployExectorContract(env);
+    await deployBridgeContract(env);
+
+    // deploy asset handlers
+    await deployERC20AssetHandlerContract(env);
+    await deployTrustlessAssetHandlerContract(env);
+
+    // registryHub set asset handlers
+    const registryHub = new ethers.Contract(env.registryHubContract, RegistryHubJson.abi, env.wallet);
+    await registryHub.setAssetHandlers(env.erc20AssetHandlerContract, '0x0000000000000000000000000000000000000000', env.trustlessAssetHandlerContract);
+    console.log('RegistryHub has set asset handlers');
+
+    // exector set bridge
+    const executor = new ethers.Contract(env.exectorContract, ExectorJson.abi, env.wallet);
+    await executor.adminSetBridge(env.bridgeContract);
+    console.log('Executor has set bridge');
+
+    // deploy staking factory contract
+    await deployStakingFactoryContract(env);
+
+    let deployCost = startBalance.sub((await env.provider.getBalance(env.wallet.address)))
+
+    console.log(`
+    ================================================================
+    Url:        ${env.url}
+    Deployer:   ${env.wallet.address}
+    Gas Limit:   ${ethers.BigNumber.from(env.gasLimit)}
+    Gas Price:   ${ethers.BigNumber.from(env.gasPrice)}
+    Deploy Cost: ${ethers.utils.formatEther(deployCost)}
+    
+    Options
+    =======
+    Bridge Fee:     ${env.bridgeFee}
+    Bridge Expiry:  ${env.bridgeExpiry}
+    Fee Addr:       ${env.feeAddr}
+    
+    Contract Addresses
+    ================================================================
+    RegistryHub:                        ${env.registryHubContract ? env.registryHubContract : "Not Deployed"}
+    ----------------------------------------------------------------
+    HomeChainAssetRegistry:             ${env.homeChainAssetRegistryContract ? env.homeChainAssetRegistryContract : "Not Deployed"}
+    ----------------------------------------------------------------
+    SteemHiveDelegateAssetRegistry:     ${env.steemHiveDelegateAssetRegistryContract ? env.steemHiveDelegateAssetRegistryContract : "Not Deployed"}
+    ----------------------------------------------------------------
+    SubstrateCrowdloanAssetRegistry:    ${env.substrateCrowdloanAssetRegistryContract ? env.substrateCrowdloanAssetRegistryContract : "Not Deployed"}
+    ----------------------------------------------------------------
+    SubstrateNominateAssetRegistry:     ${env.substrateNominateAssetRegistryContract ? env.substrateNominateAssetRegistryContract : "Not Deployed"}
+    ----------------------------------------------------------------
+    ERC20AssetHandler:                  ${env.erc20AssetHandlerContract ? env.erc20AssetHandlerContract : "Not Deployed"}
+    ----------------------------------------------------------------
+    ERC721AssetHandler:                 ${env.erc721AssetHandlerContract ? env.erc721AssetHandlerContract : "Not Deployed"}
+    ----------------------------------------------------------------
+    TrustlessAssetHandler:              ${env.trustlessAssetHandlerContract ? env.trustlessAssetHandlerContract : "Not Deployed"}
+    ----------------------------------------------------------------
+    Executor:                           ${env.exectorContract ? env.exectorContract : "Not Deployed"}
+    ----------------------------------------------------------------
+    Bridge:                             ${env.bridgeContract ? env.bridgeContract : "Not Deployed"}
+    ----------------------------------------------------------------
+    StakingFactory:                     ${env.stakingFactoryContract ? env.stakingFactoryContract : "Not Deployed"}
+    ================================================================
+            `)
+}
+
+main()
+  .catch(console.error)
+  .finally(() => process.exit());
