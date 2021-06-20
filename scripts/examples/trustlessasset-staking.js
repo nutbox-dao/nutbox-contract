f22a49f403f23c7b5779b3226ed72d3cab1f0f90
Wenfeng Wang <kalot.wang@gmail.com>
2021-06-20 21:31:27 +0800

    Do not allow process exit by themself

--- scripts/examples/trustlessasset-staking.js
+++ scripts/examples/trustlessasset-staking.js
@@ -123,6 +123,28 @@ async function main() {
         );
         await waitForTx(env.provider, tx4.hash);
     });
+
+    process.stdin.resume();//so the program will not close instantly
+
+    function exitHandler(options, exitCode) {
+        if (options.cleanup) console.log('clean');
+        if (exitCode || exitCode === 0) console.log(exitCode);
+        if (options.exit) process.exit();
+    }
+
+    // Following copy from: https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits/14032965#14032965
+    //do something when app is closing
+    process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+    //catches ctrl+c event
+    process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+    // catches "kill pid" (for example: nodemon restart)
+    process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+    process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+    //catches uncaught exceptions
+    process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
 }
 
 main();
