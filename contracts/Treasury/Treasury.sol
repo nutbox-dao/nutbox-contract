0e32b9e9d2ce1c232a68b55c60a9c945a18e69b2
terry3t <wangxi8206@sina.com>
2022-05-05 10:16:22 +0800

    add treasury

--- contracts/Treasury/Treasury.sol
+++ contracts/Treasury/Treasury.sol
@@ -8,16 +8,28 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./TreasuryFactory.sol";
 import "../interfaces/ICommunity.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract Treasury is Ownable, ReentrancyGuard {
+contract Treasury is ReentrancyGuard {
     using SafeMath for uint256;
     address immutable factory;
     address immutable community;
 
-    constructor() {
+    constructor(address _community) {
         // we only admit the caller is TreasuryFactory registered by committee
         factory = msg.sender;
+        community = _community;
     }
+
+    redeem(uint256 amount) {
+        ERC20Burnable ctoken = ERC20Burnable(ICommunity(community).getCommunityToken());
+        require(ctoken.balaceOf(msg.sender) >= amount, "Insufficient balance");
+        uint256 supply = ctoken.totalSupply();
+        ctoken.transferFrom(msg.sender, address(this), amount);
+        ctoken.burn(amount);
+    }
+    
+
 
 
 }
