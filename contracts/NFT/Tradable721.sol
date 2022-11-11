3a29c6f70a7d458da5b5b8c3304f8a90cc7fa5a0
necklace <xiangxn@163.com>
2022-11-11 16:05:08 +0800

    add mint fun

--- contracts/NFT/Tradable721.sol
+++ contracts/NFT/Tradable721.sol
@@ -3,15 +3,21 @@ pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 
 contract Tradable721 is ERC721PresetMinterPauserAutoId {
     using Strings for uint256;
+    using Counters for Counters.Counter;
 
     bytes32 public constant BURN_ROLE = keccak256("BURN_ROLE");
     bytes32 public constant URLSETTER_ROLE = keccak256("URLSETTER_ROLE");
 
-    string private _baseTokenURI;
+    string _baseTokenURI;
+    Counters.Counter _tokenIdTracker;
+
+    // token id => ipfs cid
+    mapping(uint256 => string) public cids;
 
     constructor(string memory name, string memory symbol) ERC721PresetMinterPauserAutoId(name, symbol, "") {
         _setupRole(BURN_ROLE, _msgSender());
@@ -31,11 +37,21 @@ contract Tradable721 is ERC721PresetMinterPauserAutoId {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
 
         string memory baseURI = _baseURI();
-        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : "";
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, cids[tokenId])) : "";
     }
 
     function setBaseURI(string memory uri) public virtual {
         require(hasRole(URLSETTER_ROLE, _msgSender()), "Must have URLSETTER_ROLE role to set uri");
         _baseTokenURI = uri;
     }
+
+    function adminMint(address to, string memory cid) public virtual {
+        require(hasRole(MINTER_ROLE, _msgSender()), "ERC721PresetMinterPauserAutoId: must have minter role to mint");
+        uint256 id = _tokenIdTracker.current();
+        cids[id] = cid;
+        _mint(to, id);
+        _tokenIdTracker.increment();
+    }
+
+    function mint(address to) public virtual override {}
 }
