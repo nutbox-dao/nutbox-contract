a300ef5d69108481a995b1625074945277d82ad1
necklace <xiangxn@163.com>
2022-11-25 17:56:11 +0800

    add tokensOf

--- contracts/NFT/Tradable721.sol
+++ contracts/NFT/Tradable721.sol
@@ -19,6 +19,12 @@ contract Tradable721 is ERC721PresetMinterPauserAutoId {
     // token id => ipfs cid
     mapping(uint256 => string) public cids;
 
+    //address => ids
+    mapping(address => uint256[]) internal ownerTokens;
+    mapping(uint256 => uint256) internal tokenIndexs;
+
+    // mapping(uint256 => address) internal tokenOwners;
+
     constructor(string memory name, string memory symbol) ERC721PresetMinterPauserAutoId(name, symbol, "") {
         _setupRole(BURN_ROLE, _msgSender());
         _setupRole(URLSETTER_ROLE, _msgSender());
@@ -53,5 +59,69 @@ contract Tradable721 is ERC721PresetMinterPauserAutoId {
         _tokenIdTracker.increment();
     }
 
+    function tokensOf(
+        address owner,
+        uint256 startIndex,
+        uint256 endIndex
+    ) public view virtual returns (uint256[] memory) {
+        require(owner != address(0), "owner is zero address");
+
+        uint256[] storage tokens = ownerTokens[owner];
+        if (endIndex == 0) {
+            return tokens;
+        }
+
+        require(startIndex < endIndex, "invalid index");
+
+        uint256[] memory result = new uint256[](endIndex - startIndex);
+        for (uint256 i = 0; i != endIndex - startIndex; ++i) {
+            result[i] = tokens[startIndex + i];
+        }
+
+        return result;
+    }
+
+    /**********overridden fun**********/
+
     function mint(address to) public virtual override {}
+
+    function _removeTokenFrom(address from, uint256 tokenId) internal virtual {
+        uint256 index = tokenIndexs[tokenId];
+
+        uint256[] storage tokens = ownerTokens[from];
+        uint256 indexLast = tokens.length - 1;
+
+        uint256 tokenIdLast = tokens[indexLast];
+        tokens[index] = tokenIdLast;
+        tokenIndexs[tokenIdLast] = index;
+
+        tokens.pop();
+    }
+
+    function _addTokenTo(address to, uint256 tokenId) internal virtual {
+        uint256[] storage tokens = ownerTokens[to];
+        tokenIndexs[tokenId] = tokens.length;
+        tokens.push(tokenId);
+    }
+
+    function _mint(address to, uint256 tokenId) internal virtual override {
+        super._mint(to, tokenId);
+        _addTokenTo(to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal virtual override {
+        super._burn(tokenId);
+        address owner = ownerOf(tokenId);
+        _removeTokenFrom(owner, tokenId);
+    }
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override {
+        super._transfer(from, to, tokenId);
+        _removeTokenFrom(from, tokenId);
+        _addTokenTo(to, tokenId);
+    }
 }
