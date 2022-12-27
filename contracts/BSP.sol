fe91e55b52e1d14e074a057b9f94bec8c4d9a704
necklace <xiangxn@163.com>
2022-12-27 20:19:13 +0800

    fix bsp bug

--- contracts/BSP.sol
+++ contracts/BSP.sol
@@ -9,17 +9,30 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract BSP is Ownable, AccessControlEnumerable, ERC20Burnable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    event SteemToBSP(string steemAccount, address indexed to, uint256 indexed amount);
-    event BSPToSteem(string steemAccount, address indexed to, uint256 indexed amount);
+    event SteemToBSP(
+        string steemAccount,
+        address indexed to,
+        uint256 indexed amount
+    );
+    event BSPToSteem(
+        string steemAccount,
+        address indexed to,
+        uint256 indexed amount
+    );
 
-    constructor() {
+    constructor() ERC20("BSC STEEP POWER", "BSP") {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _setupRole(MINTER_ROLE, _msgSender());
-        _name = "BSC STEEP POWER";
-        _symbol = "BSP";
+
+        // _name = "BSC STEEP POWER";
+        // _symbol = "BSP";
     }
 
-    function steemToBsp(string memory steem, address to, uint256 amount) public {
+    function steemToBsp(
+        string memory steem,
+        address to,
+        uint256 amount
+    ) public {
         // require(hasRole(MINTER_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have minter role to mint");
         require(to != address(0), "Wrong target address");
         _mint(to, amount);
