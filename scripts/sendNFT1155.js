c0a3812f8e7eaadfac04d646ac8a53c43cb47294
necklace <xiangxn@163.com>
2023-01-26 23:01:31 +0800

    fix sleep

--- scripts/sendNFT1155.js
+++ scripts/sendNFT1155.js
@@ -12,27 +12,7 @@ let cBlessCard;
 
 let sended = [];
 
-let ignore = [
-    "0x125a7a6ddf7504fe97698ca717a51125f53817b3",
-    "0x58278882433feb428031a12c2517f32377332c42",
-    "0x7a2d7696e07d868fdc250616d4af9e186ce7112b",
-    "0x787e36e1cafa19f47a6253a84758f43a04dc3b17",
-    "0x4d12c40e9c095b910eb597d16939ed5493ea77c3",
-    "0x1b02ca1e76870da2755fa080f69fd14d0f7ae37a",
-    "0xea324758383977be9355372e7be3e10f2c6d943f",
-    "0x2e90bb9061e368d0ae757009692915471dbc3949",
-    "0x44e2ab6d9203593942054cc23248fff1ea16593e",
-    "0xcb4abe454ea81404bcd43d6b67e8892a5c2405a6",
-    "0x97c23135a36d5ba18ac7878859be0936585c4746",
-    "0x1a4f00a3637d8717e18c9a2585c5cdaf4224d471",
-    "0x202bde5450105e332600a84ab555b8dd9e1d469d",
-    "0xe4fe1e8ba05e482a86c1c5664fb08f1523e0181b",
-    "0xa4f818423c2b41be30d1b9905bbe5d9d601f7799",
-    "0x74d4d5546972e2622b588185a34095b6973b8bb1",
-    "0xf42737a9d722a6b2f8b68e1359fff32afe3d5e8f",
-    "0x063b31d67dd95b376cd6de4cac4b9808f5dbd49d",
-    "0xe1abe2a3189b9ab53120aa209cc1b1c041e64eeb"
-];
+let ignore = [];
 
 async function readAddress() {
     return new Promise((resolve, reject) => {
@@ -60,14 +40,19 @@ async function saveSend() {
 
 async function readSend() {
     return new Promise((resolve, reject) => {
-        fs.readFile("./sended.json", "utf8", (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                sended = JSON.parse(data);
-                resolve();
-            }
-        });
+        let f = "./sended.json";
+        if (fs.existsSync(f)) {
+            fs.readFile("./sended.json", "utf8", (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    sended = JSON.parse(data);
+                    resolve();
+                }
+            });
+        } else {
+            resolve();
+        }
     });
 }
 
@@ -78,8 +63,9 @@ async function sendNFT(contract, from, to, id, amount) {
             let tx = await contract.safeTransferFrom(from, to, id, amount, '0x00', { gasPrice });
             sended.push(to);
             console.log(`\t${sended.length}\t| 已经发送: [${to},${id},${amount}] hash:${tx.hash}`);
+            await sleep(2000);
         } else {
-            console.log(`\t${sended.length}\t| `,to, "...");
+            console.log(`\t${sended.length}\t| `, to, "...");
         }
     } catch (e) {
         console.log(`\tsendNFT to [${to}] error: ${e}`);
@@ -115,7 +101,6 @@ async function main() {
                 else
                     console.log(addr, "...");
             }
-            await sleep(2000);
         }
         await saveSend();
     }
