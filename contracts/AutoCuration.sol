a0e4c581a4daa4515dbc56f4bdd936235b59c718
necklace <xiangxn@163.com>
2023-02-06 10:51:36 +0800

    update autocuration contract

--- contracts/AutoCuration.sol
+++ contracts/AutoCuration.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract AutoCuration is Ownable, ReentrancyGuard {
-    event RewardInfo(uint256 indexed twitterId, address indexed user, uint256[] taskIds, uint256[] amounts);
+    event RewardInfo(uint256 indexed twitterId, address indexed user, uint256[] taskIds, uint256 amount);
 
     // Flag claimed users
     // twitterid => curation id => bool
@@ -54,28 +54,24 @@ contract AutoCuration is Ownable, ReentrancyGuard {
         uint256 twitterId,
         address addr,
         uint256[] calldata curationIds,
-        uint256[] calldata amounts,
+        uint256 amount,
         bytes calldata sign
     ) public nonReentrant {
         require(curationIds.length > 0, "get at least one");
-        require(curationIds.length == amounts.length, "invalid data");
         require(sign.length == 65, "invalid sign length");
         require(addr == msg.sender, "invalid addr");
         require(prizeToken != address(0), "prize token not set");
 
-        bytes32 data = keccak256(abi.encodePacked(twitterId, chainId, addr, curationIds, amounts));
+        bytes32 data = keccak256(abi.encodePacked(twitterId, chainId, addr, curationIds, amount));
         require(_check(data, sign), "invalid sign");
 
         for (uint256 i = 0; i < curationIds.length; i++) {
             if (alreadyClaimed[twitterId][curationIds[i]] == false) {
                 alreadyClaimed[twitterId][curationIds[i]] = true;
-
-                if (amounts[i] > 0) {
-                    IERC20(prizeToken).transfer(msg.sender, amounts[i]);
-                }
             }
         }
-        emit RewardInfo(twitterId, msg.sender, curationIds, amounts);
+        IERC20(prizeToken).transfer(msg.sender, amount);
+        emit RewardInfo(twitterId, msg.sender, curationIds, amount);
     }
 
     function _check(bytes32 data, bytes calldata sign) internal view returns (bool) {
