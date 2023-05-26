a785c9da373114a9c8cfdb0818d3d891ef130096
necklace <xiangxn@163.com>
2023-05-26 11:54:14 +0800

    update deployCommunity script

--- scripts/deployCommunity.js
+++ scripts/deployCommunity.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const ethers = require('ethers');
 const { getEnv, deployContract } = require('./utils');
+const { ArgumentParser } = require('argparse');
 
 const CommunityCuration = require('../build/contracts/CommunityCuration.json');
 const ERC20PresetMinterPauser = require('../build/contracts/ERC20PresetMinterPauser.json');
@@ -9,11 +10,27 @@ const ERC20PresetMinterPauser = require('../build/contracts/ERC20PresetMinterPau
 async function main() {
     let env = await getEnv(false);
     await deployContract(env, CommunityCuration);
+    let prizeToken = args.paddr;
     if (env.chainId == 1337) {
         await deployContract(env, ERC20PresetMinterPauser, ["Test USDT", "USDT"]);
+        prizeToken = ERC20PresetMinterPauser.networks[env.chainId].address;
+    }
+    if (args.cid && args.saddr && prizeToken) {
+        // create community
+        let cCommunityCuration = new ethers.Contract(CommunityCuration.networks[env.chainId].address, CommunityCuration.abi, env.wallet);
+        await cCommunityCuration.createCommunity(args.cid, args.saddr, prizeToken);
+        let info = await cCommunityCuration.getCommunityInfo(args.cid);
+        console.log("info:", info);
     }
 }
 
+const parser = new ArgumentParser({
+    description: 'Deploy the community version curation contract'
+});
+parser.add_argument('-C', '--cid', { help: 'community id' });
+parser.add_argument('-S', '--saddr', { help: 'signature address' });
+parser.add_argument('-P', '--paddr', { help: 'prize token address' });
+const args = parser.parse_args();
 
 main()
     .catch(console.error)
