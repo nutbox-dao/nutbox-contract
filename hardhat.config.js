7dad6d5742b435408deda090a7a411d224f20369
wangxi <wangxi8206@sina.com>
2023-07-25 16:17:49 +0800

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,42 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require('hardhat-contract-sizer');
+require("@nomicfoundation/hardhat-verify");
+require('dotenv').config();
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0",
-  
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled:true,
+        runs: 1
+      },
+      evmVersion: "constantinople"
+    }
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      url: "http://localhost:8545",
+    },
+    arbitrum: {
+      url: 'https://arb1.arbitrum.io/rpc',
+      chainId: 42161,
+      accounts: [process.env.MAIN_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ARB_KEY
+  }
+  // contractSizer: {
+  //   alphaSort: true,
+  //   runOnCompile: true,
+  //   disambiguatePaths: false
+  // }
 };
