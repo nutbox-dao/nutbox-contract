ba4127dfeec789a4786cea2db6f561c7fd892ff6
necklace <xiangxn@163.com>
2023-12-07 22:04:36 +0800

    add test

--- test/test_ecrecover.js
+++ test/test_ecrecover.js
@@ -32,8 +32,38 @@ async function main() {
     await cCuration.claimPrize(twitterId, env.wallet.address, curationIds, amounts, sign);
 }
 
+async function main2() {
+    let abi = [{ "type": "function", "name": "claimPrize", "inputs": [{ "name": "twitterId", "type": "uint256", "internalType": "uint256" }, { "name": "addr", "type": "address", "internalType": "address" }, { "name": "orderId", "type": "uint256", "internalType": "uint256" }, { "name": "amount", "type": "uint256", "internalType": "uint256" }, { "name": "sign", "type": "bytes", "internalType": "bytes" }], "outputs": [], "stateMutability": "nonpayable" }, { "type": "function", "name": "claimPrize2", "inputs": [{ "name": "twitterId", "type": "uint256", "internalType": "uint256" }, { "name": "addr", "type": "address", "internalType": "address" }, { "name": "orderId", "type": "uint256", "internalType": "uint256" }, { "name": "amount", "type": "uint256", "internalType": "uint256" }, { "name": "sign", "type": "bytes", "internalType": "bytes" }], "outputs": [], "stateMutability": "nonpayable" }, { "type": "function", "name": "claimPrize3", "inputs": [{ "name": "hash", "type": "bytes32", "internalType": "bytes32" }, { "name": "sign", "type": "bytes", "internalType": "bytes" }], "outputs": [], "stateMutability": "nonpayable" }, { "type": "function", "name": "count", "inputs": [], "outputs": [{ "name": "", "type": "uint256", "internalType": "uint256" }], "stateMutability": "view" }, { "type": "function", "name": "signAddress", "inputs": [], "outputs": [{ "name": "", "type": "address", "internalType": "address" }], "stateMutability": "view" }];
+    let env = await getEnv(false);
 
-main()
+    let cCuration = new ethers.Contract("0x3Aa5ebB10DC797CAC828524e59A333d0A371443c", abi, env.wallet);
+
+    console.log("signer: ", env.wallet.address);
+
+    let twitterId = "123";
+    let curationId = "123";
+    let amount = "10000";
+
+    let data = ethers.utils.solidityKeccak256(["uint256", "address", "uint256", "uint256"], [twitterId, env.wallet.address, curationId, amount]);
+    data = ethers.utils.arrayify(data)
+
+    console.log("data: ", data.length, data);
+
+    data = ethers.utils.arrayify(data);
+    console.log("hashmsg: ", ethers.utils.hashMessage(data));
+
+    let sign = await env.wallet.signMessage(data);
+    console.log("sign: ", sign.length, sign);
+
+    let sig = ethers.utils.splitSignature(sign);
+    console.log("sig: ", sig);
+
+    // await cCuration.claimPrize2(twitterId, env.wallet.address, curationId, amount, sign);
+    await cCuration.claimPrize3(ethers.utils.hashMessage(data), sign);
+}
+
+
+main2()
     .catch(console.error)
     .finally(() => process.exit());
 
